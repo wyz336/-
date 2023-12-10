d37ee2d774b746ac3e5a6b332f96ad8fb382828a
52makima <152400248+52makima@users.noreply.github.com>
2023-12-10 19:10:10 +0800

    Add files via upload

---
+++ YFDAL/CommentMapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YF.MsSqlHelper;
+using YFModel;
+
+namespace YFDAL
+{
+    public class CommentMapper
+    {
+        public static int insert(Comment comment)
+        {
+            string sql= "insert into t_comment(userid,comment,bookid,username) values("+comment.userid+",'"+comment.comment+"',"+comment.bookid+","+"'"+comment.username+"')";
+            int res=YFMsSqlHelper.ExecuteSql(sql);
+            return res;
+        }
+    }
+}
