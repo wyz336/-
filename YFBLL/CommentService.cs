f035432c6f96152af1e466daae597c0ea233e8f5
cblx-sx <153609661+cblx-sx@users.noreply.github.com>
2023-12-12 18:29:52 +0800

    Add files via upload

---
+++ YFBLL/CommentService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YFDAL;
+using YFModel;
+
+namespace YFBLL
+{
+    public class CommentService
+    {
+        public static int insert(Comment comment)
+        {
+            return CommentMapper.insert(comment);
+        }
+    }
+}
