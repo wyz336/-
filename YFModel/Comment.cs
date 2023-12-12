cd8b5365b46ae7ea56aea67c9e6588cc67e7bbe7
ooketch <134914744+ooketch@users.noreply.github.com>
2023-12-12 15:39:10 +0800

    Add files via upload

---
+++ YFModel/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YFModel
+{
+    public  class Comment
+    {
+        public string username;
+        public string comment;
+        public  int bookid;
+        public int userid;
+    }
+}
